a4d5063ccb6cc48afcf4610e3e54b4ec112314b2
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-29 22:54:37 +0530

    createNewEntry fun added

--- Notes App/ViewController.swift
+++ Notes App/ViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textView: UITextView!
+    
+    var moc : NSManagedObjectContext!
+    var entry : NSManagedObject!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            moc = appDelegate.persistentContainer.viewContext
+        
+        if entry != nil{
+            self.textView.text = entry.value(forKey: "bodyText") as? String
+        } else {
+            self.textView.text = ""
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        if entry != nil{
+            self.updateEntry()
+        } else {
+            if textView.text != nil{
+                self.createNewEntry()
+            }
+        }
+    }
+    
+    func updateEntry(){
+        
+    }
+    
+    func createNewEntry(){
+        let entryEntity = NSEntityDescription.entity(forEntityName: "Note", in:  moc)!
+        
+        let entryObject = NSManagedObject(entity: entryEntity, insertInto: moc)
+        
+        entryObject.setValue(self.textView.text, forKey: "bodyText")
+        
+        entryObject.setValue(Date(), forKey: "createdAt")
+        
+        do{
+            try moc.save()
+        } catch let error as NSError{
+            print(error.localizedDescription, "Could not save")
+        }
     }
 
 
